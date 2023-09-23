8382ee3ec00b54be4261a49408dc2e6e82b0c0ed
Muhammad Abdullah Qureshi <67544354+maq796113@users.noreply.github.com>
2023-09-24 01:08:06 +0500

    Update keep_it_running.py

--- keep_it_running.py
+++ keep_it_running.py
@@ -1,15 +1,11 @@
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Assalamualikum. I am here!"
 
-def run():
-    app.run(host = '0.0.0.0', port = 8080)
-
-def keep_running():
-    t = Thread(target = run)
-    t.start()
+def create_app():
+   return app
+    
