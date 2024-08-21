6c2e7d79e45067926db48f592a200d6e2dd3ea24
Muhammad Abdullah Qureshi <67544354+maq796113@users.noreply.github.com>
2024-08-21 22:39:44 +0500

    Update getHadith.py

--- getHadith.py
+++ getHadith.py
@@ -1,57 +1,73 @@
-import discord
-from discord import app_commands
-import typing
+import html2text
+import requests
+import settings
+import arabic_reshaper
 
-class GreetingsGroup(app_commands.Group):
-  
-    async def gender_autocomplete(
-        self,
-        interaction: discord.Interaction,
-        current: str
-    ) -> typing.List[app_commands.Choice[str]]:
-        data = []
-        for collection_choice in ["male", "female"]:
-            if current.lower() in collection_choice.lower():
-                data.append(app_commands.Choice(name=collection_choice, value=collection_choice))
-        return data
-    
-    @app_commands.command(description="Say Salam in Arabic")
-    @app_commands.autocomplete(gender=gender_autocomplete)
-    async def arabic(self, interaction: discord.Interaction, gender: str, *, member: discord.Member):
-        if gender == "male":     
-            embed = discord.Embed(
-                title="Akhi",
-                description=f'*ٱلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ ٱللَّٰهِ وَبَرَكَاتُهُ* {member.mention}',
-                color=discord.Color.gold()
+logger = settings.logging.getLogger("bot")
+
+
+def get_hadith(book_collection: str, book_num: int, hadith_num: int):
+    h = html2text.HTML2Text()
+    h.ignore_links = True
+    h.ignore_images = True
+    h.ignore_tables = True
+
+    payload = {"content-type": "application/json"}
+    headers = {"x-api-key": settings.HADITH_API_TOKEN}
+    request = requests.get(
+        f"https://api.sunnah.com/v1/collections/{book_collection}/books/{book_num}/hadiths",
+        params=payload,
+        headers=headers,
+    )
+    hadith_dict = request.json()
+    _iter = 0
+
+    for data in hadith_dict["data"]:
+
+        if _iter == hadith_num - 1:
+
+            hadith_chap_title_eng = data["hadith"][0]["chapterTitle"]
+
+            hadith_eng = h.handle(data["hadith"][0]["body"]).replace("\n", "")
+
+            if data["hadith"][0]["grades"]:
+                grading = data["hadith"][0]["grades"][0]["grade"]
+            else:
+                grading = data["hadith"][1]["grades"][0]["grade"] + f"({data['hadith'][1]['grades'][0]['graded_by']})"
+
+            hadith_chap_title_ar = data["hadith"][1]["chapterTitle"]
+
+            hadith_ar = h.handle(data["hadith"][1]["body"]).replace("\n", "").replace("‏.‏", "")
+            reshaped_hadith_ar = arabic_reshaper.reshape(hadith_ar)
+
+            hadith_num = data["hadithNumber"]
+            chapter_num = data["chapterId"].replace(".00", "")
+
+            if '\"' in hadith_eng:
+                hadith_eng = put_asterisk_in_quotes(hadith_eng)
+
+            return (
+                reshaped_hadith_ar,
+                hadith_eng,
+                grading,
+                hadith_num,
+                hadith_chap_title_eng,
+                hadith_chap_title_ar,
+                chapter_num
             )
-            await interaction.response.send_message(embed=embed)
+
         else:
-            embed = discord.Embed(
-                title="Ukhti",
-                description=f'*ٱلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ ٱللَّٰهِ وَبَرَكَاتُهُ* {member.mention}',
-                color=discord.Color.gold()
-            )
-            await interaction.response.send_message(embed=embed)
-
-    @app_commands.command(description="Say Salam in English")
-    @app_commands.autocomplete(gender=gender_autocomplete)
-    async def english(self, interaction: discord.Interaction, gender: str, *, member: discord.Member):
-        
-        if gender == "male":
-            embed = discord.Embed(
-                title='Akhi',
-                description=f'*As-salāmu ʿalaykum wa-raḥmatu -llāhi wa-barakātuhū* {member.mention}',
-                color=discord.Color.gold()
-            )
-            await interaction.response.send_message(embed=embed)
-        else:
-            embed = discord.Embed(
-                title='Ukhti',
-                description=f'*As-salāmu ʿalaykum wa-raḥmatu -llāhi wa-barakātuhū* {member.mention}',
-                color=discord.Color.gold()
-            )
-            await interaction.response.send_message(embed=embed)
+            _iter += 1
 
 
-async def setup(bot):
-    bot.tree.add_command(GreetingsGroup(name="salam", description="Says Salam to the user"))
+def put_asterisk_in_quotes(text: str) -> str:
+    words = text.split()
+
+    for i, word in enumerate(words):
+        if word[0] == '\"':
+            words[i] = words[i].replace('\"', '**\"')
+
+        if word[-1] == '\"':
+            words[i] = words[i].replace('\"', '\"**')
+
+    return ' '.join(words)
